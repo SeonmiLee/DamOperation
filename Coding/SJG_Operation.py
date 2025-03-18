eabca04b57853b08d3bbdf3f9a86b25b1b02d00f
SeonmiLee <128132864+SeonmiLee@users.noreply.github.com>
2025-03-18 14:39:56 +0900

    Update SJG_Operation.py

--- Coding/SJG_Operation.py
+++ Coding/SJG_Operation.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def SJG_Operation(WLSTO_SJG, Inflow_SJG, STO_SJG, NHSTO_SJG, LSTO_SJG, Intake_SJG, Stream_SJG):
     
-
+# SJG Operation
     if STO_SJG + Inflow_SJG - (Intake_SJG + Stream_SJG) >= LSTO_SJG:
         Supply_SJGStream = Stream_SJG
         Supply_SJGIntake = Intake_SJG
@@ -14,7 +14,7 @@ def SJG_Operation(WLSTO_SJG, Inflow_SJG, STO_SJG, NHSTO_SJG, LSTO_SJG, Intake_SJ
         Supply_SJGStream = (STO_SJG + Inflow_SJG - LSTO_SJG)*(Stream_SJG / (Intake_SJG + Stream_SJG))
         Supply_SJGIntake = (STO_SJG + Inflow_SJG - LSTO_SJG)*(Intake_SJG / (Intake_SJG + Stream_SJG))
         Sp_SJG = 0
-
+# 다음 타임 저류량 저장
     STO_SJG = STO_SJG + Inflow_SJG - Supply_SJGStream - Supply_SJGIntake - Sp_SJG
     WL_SJG = np.interp(STO_SJG, WLSTO_SJG['STO'], WLSTO_SJG['WL'])
 
